d4d1a20845b46eec2c173972a72b3b4fc67449de
Hugo Faria <hugofaria157@live.com>
2022-05-18 23:00:16 -0300

    simplificando o codigo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,16 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
+  ) {}
 
-  constructor(data, quantidade, valor){
-    this.#data = data
-    this.#quantidade = quantidade
-    this.#valor = valor
+  get volume(): number {
+    return this.quantidade * this.valor
   }
 
-  get data(){
-    return this.#data
-  }
-
-  get quantidade(){
-    return this.#quantidade
-  }
-
-  get valor(){
-    return this.#valor
-  }
-
-  get volume(){
-    return this.#quantidade * this.#valor
+  get data(): Date {
+    const data = new Date(this._data.getTime())
+    return data
   }
 }
